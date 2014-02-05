f560d08156c0797501754ea76a4fd963c82ae9b4
Nate Roy <nroy@twitter.com>
2014-02-05 17:59:55 -0500

    remove tabs

--- FileCreator.java
+++ FileCreator.java
@@ -9,34 +9,34 @@ import java.util.Arrays;
  * Usage : FileCreator file_size in bytes name of file
  */
 public class FileCreator {
-	
-	public static void main(String[] args) {
-		
-		if (args == null || args.length != 2) {
-			System.out.println("Usage: FileCreator <file_size> <file_name>");
-			System.exit(1);
-			return;
-		}
-		
-		final int fileSize = Integer.parseInt(args[0]);
-		final File file = new File(args[1]);
-		
-		if (file.isDirectory()) {
-			System.out.println("Nice try, not gonna delete that directory.");
-			System.exit(1);
-			return;
-		}
+    
+    public static void main(String[] args) {
+        
+        if (args == null || args.length != 2) {
+            System.out.println("Usage: FileCreator <file_size> <file_name>");
+            System.exit(1);
+            return;
+        }
+        
+        final int fileSize = Integer.parseInt(args[0]);
+        final File file = new File(args[1]);
+        
+        if (file.isDirectory()) {
+            System.out.println("Nice try, not gonna delete that directory.");
+            System.exit(1);
+            return;
+        }
 
-		if (file.exists()) {
-			file.delete();
-		}
-		final byte[] bytes = new byte[fileSize];
-		Arrays.fill(bytes, (byte) 1);
-		try {
-			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
-			bos.write(bytes);
-			bos.flush();
-			bos.close();
-		} catch (IOException e) {}
-	}
+        if (file.exists()) {
+            file.delete();
+        }
+        final byte[] bytes = new byte[fileSize];
+        Arrays.fill(bytes, (byte) 1);
+        try {
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+            bos.write(bytes);
+            bos.flush();
+            bos.close();
+        } catch (IOException e) {}
+    }
 }
